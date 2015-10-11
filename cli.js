57a320376bde40fb679fc938cc20766c3f3e1f85
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-11 15:39:36 +0700

    add `fullname-native` as an optional dependency

--- cli.js
+++ cli.js
@@ -4,10 +4,21 @@
 const meow = require('meow');
 const fullname = require('fullname');
 
+let fullnameNative;
+
+try {
+	fullnameNative = require('fullname-native');
+} catch (err) {}
+
 meow(`
 	Example
 	  $ fullname
 	  Sindre Sorhus
 `);
+
+if (fullnameNative) {
+	console.log(fullnameNative);
+	process.exit();
+}
 
 fullname().then(console.log);
